a507386f65723f3268afb64afb3ffd75eb5b7a3f
Shao Liang <th.synee@gmail.com>
2013-03-16 19:10:43 +0800

    initial commit

---
+++ node_module/penet/lib/penet.js
@@ -0,0 +1,30 @@
+var http = require("http");
+var fs   = require("fs");
+
+var Penet  = function(options){
+    this.VERSION = "0.0.1";
+
+    this.static = function(dir){
+        this.static_dir = dir;
+        console.log(this);
+    };
+
+    this.start = function(port){
+        var me = this;
+        var server = http.createServer(function(requst, response){
+            response.writeHead(200, {"Content-Type": "text/html"});
+            console.log(me.static_dir + requst.url);
+            fs.readFile(me.static_dir + requst.url, {
+                encoding: "UTF-8"
+            },function(err, data){
+                response.write(new Buffer(data).toString("UTF-8"));
+                response.end();
+            });
+        });
+        if(!port) port = 3000;
+        server.listen(port);
+    };
+};
+
+module.exports = Penet;
+
---
+++ node_module/penet/lib/util.js
@@ -0,0 +1,6 @@
+util = {}
+
+util.extend = function(target, src){
+    if(typeof target != "Object") throw new TypeError("Target is not an object");
+    src.ke
+}
