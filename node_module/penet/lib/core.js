5eda50b7ea31a5437dde1862cbb6881966bea737
Shao Liang <th.synee@gmail.com>
2013-03-18 00:24:02 +0800

    PENET
    parse params

--- node_module/penet/lib/core.js
+++ node_module/penet/lib/core.js
@@ -6,10 +6,34 @@ var url = require("url");
 
 
 var data2JSON = function(requestData){
+
     var params = {};
-    for(k in requestData){
-        params[k] = requestData[k];
+    var loop = function(strArr, value, index){
+        var obj;
+        if(index == strArr.length -1){
+
+        }else{
+            obj = value;
+        }
+        return obj;
     }
+    /**
+     * 将每一行的 “[” “]” 替换成 “.” ,
+     * { model: 'user',
+     *    fn: 'save',
+     *    'message[result]': 'true',
+     *    'message[code]': '200',
+     *    'message[message][result]': 'true',
+     *    'message[message][code]': '200',
+     *    'message[message][message]': '[信息]' }
+     *
+     */
+    for(key in requestData){
+        params[key.split("[")[0]] = loop(key.split("["),requestData,key.split("[").length - 1);
+    }
+
+    console.log(params);
+
     return params;
 }
 
@@ -21,7 +45,7 @@ module.exports = {
         if (request.method = "GET"){
             requestData = url.parse(request.url).query;
             params = data2JSON(queryString.parse(requestData));
-            end(queryString.parse(postData));
+            end(queryString.parse(requestData));
         } else if(request.method = "POST"){
             request.addListener("data", function(postDataChunk) {
                 requestData += postDataChunk;
